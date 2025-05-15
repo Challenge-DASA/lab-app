637c715364dc4346a31e2cf53466098f0b7b57fd
ulipese <felipe.sousa4030@gmail.com>
2025-05-14 22:13:53 -0300

    feat: implement better stylish and structure to home

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.dasa.challenge.labapp {
     requires javafx.fxml;
 
 
-    opens com.dasa.challenge.labapp.controllers to javafx.fxml;
+    opens com.dasa.challenge.labapp.controllers.components to javafx.fxml;
     exports com.dasa.challenge.labapp;
+    opens com.dasa.challenge.labapp.controllers.pages to javafx.fxml;
 }

4afc36a3ec85e60c9f51cd7697f57a10c8bff4c1
ulipese <felipe.sousa4030@gmail.com>
2025-05-14 22:14:28 -0300

    refactor: changes slider to better animation and flow

---
+++ src/main/java/com/dasa/challenge/labapp/utils/SliderSwitch.java
@@ -0,0 +1,59 @@
+package com.dasa.challenge.labapp.utils;
+
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.Objects;
+
+public class SliderSwitch {
+    public static void slide(Stage stage, String fxmlPath, String cssPath) {
+        try {
+            new Thread(() -> {
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(SliderSwitch.class.getResource(fxmlPath));
+                    Parent newRoot = fxmlLoader.load();
+
+                    javafx.application.Platform.runLater(() -> {
+                        StackPane rootContainer = (StackPane) stage.getScene().getRoot();
+                        Parent currentRoot = (Parent) rootContainer.getChildren().get(0);
+
+                        newRoot.setTranslateX(stage.getWidth());
+                        rootContainer.getChildren().add(newRoot);
+
+                        TranslateTransition slideOut = new TranslateTransition(Duration.millis(500), currentRoot);
+                        slideOut.setFromX(0);
+                        slideOut.setToX(-stage.getWidth());
+
+                        TranslateTransition slideIn = new TranslateTransition(Duration.millis(500), newRoot);
+                        slideIn.setFromX(stage.getWidth());
+                        slideIn.setToX(0);
+
+                        ParallelTransition transition = new ParallelTransition(slideOut, slideIn);
+                        transition.setOnFinished(event -> {
+                            rootContainer.getChildren().remove(currentRoot);
+                        });
+
+                        Scene scene = stage.getScene();
+                        scene.getStylesheets().add(Objects.requireNonNull(
+                                SliderSwitch.class.getResource(cssPath)).toExternalForm()
+                        );
+
+                        transition.play();
+                    });
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
